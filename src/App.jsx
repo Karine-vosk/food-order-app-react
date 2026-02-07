756627e72e762f4bd494f0602fae9370b7ef8928
Karine-vosk <karinevoskanyan.vk@gmail.com>
2026-02-07 14:49:50 +0400

    finished project

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import { UserProgressProvider } from './store/UserProgressContext.jsx';
 import Meals from './components/Meals.jsx';
 import Cart from './components/Cart/Cart.jsx';
 import Checkout from './components/Checkout.jsx';
-import Header from './components/Header.jsx';
+import CustomHeader from './components/CustomHeader.jsx';
 
 function App() {
   return (
     <UserProgressProvider>
       <CartContextProvider>
-        <Header />
+        <CustomHeader />
         <Meals />
         <Cart />
         <Checkout />
