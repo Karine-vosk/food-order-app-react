e2b4a90a8ab2e18b7985acc4349239daf55d825a
Karine-vosk <karinevoskanyan.vk@gmail.com>
2026-02-07 13:45:04 +0400

    problem with deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
+import { CartContextProvider } from './store/CartContext.jsx';
+import { UserProgressProvider } from './store/UserProgressContext.jsx';
 import Header from './components/header.jsx';
 import Meals from './components/Meals.jsx';
 import Cart from './components/Cart/Cart.jsx';
-import { CartContextProvider } from './store/CartContext.jsx';
-import { UserProgressProvider } from './store/UserProgressContext.jsx';
 import Checkout from './components/Checkout.jsx';
 
 function App() {
